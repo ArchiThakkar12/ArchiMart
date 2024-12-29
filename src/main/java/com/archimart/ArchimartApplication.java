d107cefdf36e9f09fc8098dcdb71297e38b12ebb
Archi <Thakkararchi3@gmail.com>
2024-12-29 12:28:32 +0530

    Added spring starter template

---
+++ src/main/java/com/archimart/ArchimartApplication.java
@@ -0,0 +1,13 @@
+package com.archimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchimartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchimartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archimart/ArchimartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archimart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchimartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
