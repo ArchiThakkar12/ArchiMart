e7c6d7f46e9fa7b404c334d223488445b2be4e63
Archi <Thakkararchi3@gmail.com>
2025-01-07 01:15:31 +0530

    Repository Annotation Added

--- src/main/java/com/archimart/repository/CustomerCartRepository.java
+++ src/main/java/com/archimart/repository/CustomerCartRepository.java
@@ -2,8 +2,11 @@ package com.archimart.repository;
 
 import com.archimart.entity.CustomerCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository(value = "customerCartRepository")
 public interface CustomerCartRepository extends JpaRepository<CustomerCart, Integer> {
 
     Optional<CustomerCart> findByCustomerEmailId(String customerEmailId);
